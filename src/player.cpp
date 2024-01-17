a2691d807a4b1d025f8086b8ffbd7a17a7d08061
Arc676/Alessandro Vinciguerra <alesvinciguerra@gmail.com>
2024-01-17 14:13:09 +0100

    Show path walked by player

--- src/player.cpp
+++ src/player.cpp
@@ -12,6 +12,28 @@ void Player::render(SDL_Renderer* const renderer,
                     const TileSettings& ts) const {
 	const int tileSize = (int)ts.tileSize;
 	const int size     = (int)ts.getHallwayWidth();
+
+	auto renderPath = [&](const Path& path, const Color& color) {
+		setRenderColor(renderer, color);
+		SDL_Rect rect{0, 0, size, size};
+		for (const auto& tile : path) {
+			rect.x = tile.x * tileSize;
+			rect.y = tile.y * tileSize;
+			SDL_RenderFillRect(renderer, &rect);
+		}
+	};
+
+	// show backtracked tiles
+	if (showBacktrack) {
+		renderPath(backtracked, backtrackColor);
+	}
+
+	// show path to current position
+	if (showPath) {
+		renderPath(path, pathColor);
+	}
+
+	// render player
 	SDL_Rect rect{x * tileSize, y * tileSize, size, size};
 	setRenderColor(renderer, playerColor);
 	SDL_RenderFillRect(renderer, &rect);
@@ -19,6 +41,25 @@ void Player::render(SDL_Renderer* const renderer,
 
 void Player::reset() {
 	x = y = 0;
+	path.clear();
+	path.push_back({0, 0});
+	backtracked.clear();
+}
+
+void Player::trackMove() {
+	SDL_Point now{x, y};
+	if (path.size() < 2) {
+		path.push_back(now);
+		return;
+	}
+	SDL_Point last   = path.back();
+	SDL_Point before = path.at(path.size() - 2);
+	if (now.x == before.x && now.y == before.y) {
+		backtracked.push_back(last);
+		path.pop_back();
+	} else {
+		path.push_back(now);
+	}
 }
 
 void Player::processEvent(const SDL_Event* const event,
@@ -30,23 +71,27 @@ void Player::processEvent(const SDL_Event* const event,
 					if (y > 0
 					    && (maze.at(x, y - 1) & Mazes::BOTTOM_WALL) == 0) {
 						y--;
+						trackMove();
 					}
 					break;
 				case SDL_SCANCODE_A:
 					if (x > 0 && (maze.at(x - 1, y) & Mazes::RIGHT_WALL) == 0) {
 						x--;
+						trackMove();
 					}
 					break;
 				case SDL_SCANCODE_S:
 					if (y < maze.getHeight() - 1
 					    && (maze.at(x, y) & Mazes::BOTTOM_WALL) == 0) {
 						y++;
+						trackMove();
 					}
 					break;
 				case SDL_SCANCODE_D:
 					if (x < maze.getWidth() - 1
 					    && (maze.at(x, y) & Mazes::RIGHT_WALL) == 0) {
 						x++;
+						trackMove();
 					}
 					break;
 				case SDL_SCANCODE_R:
--- src/player.h
+++ src/player.h
@@ -4,8 +4,10 @@
 #include <SDL2/SDL_events.h>
 #include <SDL2/SDL_render.h>
 #include <SDL_pixels.h>
+#include <SDL_rect.h>
 
 #include <array>
+#include <vector>
 
 #include "maze.h"
 #include "src/settings.h"
@@ -14,12 +16,21 @@ class Player {
 	int x = 0;
 	int y = 0;
 
+	bool showPath      = true;
+	bool showBacktrack = true;
+
 	// NOLINTBEGIN(*magic-numbers)
 	using Color = std::array<float, 4>;
 	Color playerColor{0, 1, 0, 1};
-	Color traceColor{0.5, 1, 0.5, 1};
+	Color pathColor{0.5, 1, 0.5, 1};
 	Color backtrackColor{1, 0.5, 0.5, 1};
 	// NOLINTEND(*magic-numbers)
+
+	using Path = std::vector<SDL_Point>;
+	Path path{{0, 0}};
+	Path backtracked{};
+
+	void trackMove();
 
 public:
 	void render(SDL_Renderer* renderer, const TileSettings& ts) const;
